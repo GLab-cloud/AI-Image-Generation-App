d06d46996cfbb79e43a09ddb90841e6df8531c9f
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-08-19 06:05:35 +0000

    first commit

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CreatePost = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline text-center"> Create Post</h1>
+    </div>
+  );
+};
+export default CreatePost;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline text-center"> Home</h1>
+    </div>
+  );
+};
+export default Home;
