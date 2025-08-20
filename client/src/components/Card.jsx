85e0af60c700c66a82e951a2b2d99b80c2e68af8
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-08-20 08:43:32 +0000

    Home page UI

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center"> Card</h1>
+    </div>
+  );
+};
+export default Card;
---
+++ client/src/components/FormField.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FormField = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center"> FormField</h1>
+    </div>
+  );
+};
+export default FormField;
